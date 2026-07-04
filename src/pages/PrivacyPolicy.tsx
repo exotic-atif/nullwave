646ff99e0315959f1c6829febeacdf12d2fb5172
Atif <mratif00007@gmail.com>
2026-07-04 21:16:40 +0530

    Fix unused import

--- src/pages/PrivacyPolicy.tsx
+++ src/pages/PrivacyPolicy.tsx
@@ -1,5 +1,5 @@
 import { motion } from 'framer-motion'
-import { Shield, Lock, Eye, Database, ArrowLeft } from 'lucide-react'
+import { Shield, Lock, Database, ArrowLeft } from 'lucide-react'
 import { Link } from 'react-router-dom'
 
 export function PrivacyPolicy() {
